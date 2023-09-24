3f0cbdc589f7741a877b481222c0fbbff72b8cc8
aeither <36173828+aeither@users.noreply.github.com>
2023-09-24 11:54:24 +0300

    deno bot

---
+++ src/deps.ts
@@ -0,0 +1,5 @@
+export * from 'https://deno.land/x/grammy/mod.ts'
+export type { BotCommand } from 'https://deno.land/x/grammy/types.deno.ts'
+
+export * from 'https://deno.land/std/dotenv/mod.ts'
+export * from 'https://deno.land/std/http/server.ts'
---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import { bot } from './src/bot.js'
+import { webhookCallback } from './src/deps.js'
+
+const handleUpdate = webhookCallback(bot, 'std/http')
+
+Deno.serve(async (req) => {
+  if (req.method == 'POST') {
+    const url = new URL(req.url)
+    if (url.pathname.slice(1) == bot.token) {
+      try {
+        return await handleUpdate(req)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+  return new Response()
+})
