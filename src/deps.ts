559cbb7c7642138d342c8a2e00c94a5b42f85806
aeither <36173828+aeither@users.noreply.github.com>
2023-09-24 12:30:26 +0300

    add v

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
-export * from 'https://deno.land/x/grammy/mod.ts'
-export type { BotCommand } from 'https://deno.land/x/grammy/types.deno.ts'
+export * from "https://deno.land/x/grammy@v1.18.3/mod.ts"
+export type { BotCommand } from "https://deno.land/x/grammy@v1.18.3/types.deno.ts"
 
-export * from 'https://deno.land/std/dotenv/mod.ts'
-export * from 'https://deno.land/std/http/server.ts'
+export * from "https://deno.land/std@0.202.0/dotenv/mod.ts"
+export * from "https://deno.land/std@0.202.0/http/server.ts"
