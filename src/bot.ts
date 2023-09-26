a30b1cd594dd2896178c1a108cd0ee64b4439be3
aeither <36173828+aeither@users.noreply.github.com>
2023-09-26 17:42:53 +0300

    add inline open webapp

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,5 @@
 import { TELEGRAM_BOT_TOKEN } from '../lib/constants.ts'
-import { Bot } from './deps.ts'
+import { Bot, InlineKeyboard } from './deps.ts'
 
 export const bot = new Bot(TELEGRAM_BOT_TOKEN, {
   client: {
@@ -14,6 +14,10 @@ export const bot = new Bot(TELEGRAM_BOT_TOKEN, {
 /**
  * Bot Commands
  */
-bot.command('start', (ctx) => ctx.reply('Welcome!'))
+
+const inlineKeyboard = new InlineKeyboard()
+inlineKeyboard.webApp('Expansai', 'https://expansai.vercel.app')
+
+bot.command('start', (ctx) => ctx.reply('Welcome!', { reply_markup: inlineKeyboard }))
 
 bot.command('ping', (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`))
